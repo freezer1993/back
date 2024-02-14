61a4d4b4f98db1ab1aa4aef01c457f247fdccfb8
Luis Fleitas <luisfleitas1993@outlook.com>
2024-02-14 17:45:00 -0300

    conf linux

--- config/conexion.js
+++ config/conexion.js
@@ -1,26 +1,45 @@
 const { logger } = require('./loggin');
 const dotenv = require('dotenv').config();
-const { CONNECSTRING, USERADMIN, PASSADMIN, INSTANTCLIENT} = require('./config');
+const { CONNECSTRING, USERADMIN, PASSADMIN, INSTANTCLIENT, INSTANT_CLIENT_LINUX, SERVER} = require('./config');
 const oracledb = require('oracledb');
 const NAMESPACE = 'conexion';
 const { decrypt } = require('./crypto');
 
 let connection;
 
-
-oracledb.initOracleClient({ libDir: INSTANTCLIENT });
+if (SERVER==='WINDOWS'){
+   oracledb.initOracleClient({ libDir: INSTANTCLIENT });
+}
 
 exports.connect= async function () {
    // var decrypted = decrypt(PASSADMIN);
-    try {
-        connection = await oracledb.getConnection({
-            user: USERADMIN,
-            password: PASSADMIN,
-            connectString: CONNECSTRING
-        });
-        logger.info(`${NAMESPACE} - Conexión establecida`)
-    } catch (err) {
-        logger.error(`${NAMESPACE} - Error al conectar: ${err} user: ${USERADMIN}, conexion: ${CONNECSTRING}`)
+    if (SERVER='WINDOWS'){
+        try {
+            connection = await oracledb.getConnection({
+                user: USERADMIN,
+                password: PASSADMIN,
+                connectString: CONNECSTRING
+            });
+            logger.info(`${NAMESPACE} - Conexión establecida`);
+            return connection;
+        } catch (err) {
+            logger.error(`${NAMESPACE} - Error al conectar: ${err} user: ${USERADMIN}, conexion: ${CONNECSTRING}`);
+            throw new Error(`${NAMESPACE} - Error al conectar: ${err} user: ${USERADMIN}, conexion: ${CONNECSTRING}`);
+        }
+    } else if (SERVER='LINUX'){
+        try {
+            process.env.LD_LIBRARY_PATH=INSTANT_CLIENT_LINUX;
+            connection = await oracledb.getConnection({
+                user: USERADMIN,
+                password: PASSADMIN,
+                connectString: CONNECSTRING
+            });
+            logger.info(`${NAMESPACE} - Conexión establecida`);
+            return connection;
+        } catch (err) {
+            logger.error(`${NAMESPACE} - Error al conectar: ${err} user: ${USERADMIN}, conexion: ${CONNECSTRING}`);
+            throw new Error(`${NAMESPACE} - Error al conectar: ${err} user: ${USERADMIN}, conexion: ${CONNECSTRING}`);
+        }
     }
-    return connection;
+
 }
